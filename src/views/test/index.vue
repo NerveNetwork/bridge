50f740b4f43f6a125a869493e5db5936fdbb0dac
huangsheng912 <631405221@qq.com>
2022-03-11 09:47:54 +0800

    update

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="test-nuls" style="text-align: center;padding-top: 20px">
-    <el-button @click="handleClisk">点我</el-button>
+  <div class="test-nuls" style="">
+    <div>
+      <div class="flex">
+        <span>nuls数量：</span><el-input v-model="nulsVal"></el-input>
+      </div>
+      <div class="flex">
+        <span>usdt数量：</span><el-input v-model="usdtVal"></el-input>
+      </div>
+    </div>
+    <el-button @click="handleClisk">确认</el-button>
   </div>
 </template>
 
@@ -9,29 +17,47 @@
 export default {
   data() {
     return {
-
+      nulsVal: '',
+      usdtVal: ''
     };
   },
 
   methods: {
     async handleClisk() {
-      const nabox = window.nabox;
-      const data = {
-        from: nabox.selectedAddress,
-        value: 1,
-        contractAddress: "tNULSeBaN58rJgMb1oGbjpDhH7yDRatY2ZVbU5",
-        methodName: "receiveAllAssets",
-        methodDesc: "",
-        args: [],
-        multyAssetValues: [["1", 5, 1]]
+      try {
+        const nabox = window.nabox;
+        const data = {
+          from: nabox.selectedAddress,
+          value: this.nulsVal,
+          contractAddress: "tNULSeBaN58rJgMb1oGbjpDhH7yDRatY2ZVbU5",
+          methodName: "receiveAllAssets",
+          methodDesc: "",
+          args: [],
+          multyAssetValues: [[this.usdtVal, 5, 74]]
+        }
+        const res = await nabox.contractCall(data) // 返回交易hash
+        this.$message.success(res);
+      } catch (e) {
+        this.$message.error(e)
       }
-      const res = await nabox.contractCall(data) // 返回交易hash
     }
   }
 }
 </script>
 
 <style lang="less">
-
+.test-nuls {
+  padding: 20px 20px 0;
+  text-align: center;
+  .flex {
+    display: flex;
+    align-items: center;
+    margin-bottom: 15px;
+    span {
+      width: 120px;
+      margin-right: 10px;
+    }
+  }
+}
 
 </style>
